7f997b5f4e884ed9db5a13e3c1ec56cd121b665a
Jhonjandrysdev <jhonjandrysramirez10@gmail.com>
2024-11-29 11:34:46 -0500

    add file of exericises_object_methods

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import classPromises from './promises.js'
 import './setInterval.js'
 import './setTimeOut.js'
 import './promises_exercises.js'
-
+import './exericises_objects_methods.js'
 classPromises()
